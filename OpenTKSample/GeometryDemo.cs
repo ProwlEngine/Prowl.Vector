90617f0a834c21c2c83b4fa24641b18c096f65de
Wulferis <acer1171090@gmail.com>
2025-06-11 13:10:38 +1000

    Transform 2D and Transform 3D Templates

--- OpenTKSample/GeometryDemo.cs
+++ OpenTKSample/GeometryDemo.cs
@@ -1,12 +1,6 @@
 ﻿// This file is part of the Prowl Game Engine
 // Licensed under the MIT License. See the LICENSE file in the project root for details.
 
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 using Prowl.Vector;
 using Prowl.Vector.Geometry;
 

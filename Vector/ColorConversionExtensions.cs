de414964ddd3dc7877c27448008b9988dd254067
Wulferis <acer1171090@gmail.com>
2025-08-17 12:16:27 +1000

    Added System.Drawing.Color support for Float3, Float4, Byte3, Byte4 types

--- Vector/ColorConversionExtensions.cs
+++ Vector/ColorConversionExtensions.cs
@@ -1,4 +1,5 @@
-﻿using System.Runtime.CompilerServices;
+﻿using System.Drawing;
+using System.Runtime.CompilerServices;
 
 namespace Prowl.Vector
 {
@@ -58,6 +59,52 @@ namespace Prowl.Vector
                 vector.Y / 255f,
                 vector.Z / 255f,
                 vector.W / 255f
+            );
+        }
+
+        /// <summary>
+        /// Converts a Byte4 color to System.Drawing.Color.
+        /// </summary>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static Color ToColor(this Byte4 vector)
+        {
+            return Color.FromArgb(vector.W, vector.X, vector.Y, vector.Z);
+        }
+
+        /// <summary>
+        /// Converts a Float4 color to System.Drawing.Color.
+        /// </summary>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static Color ToColor(this Float4 vector)
+        {
+            return Color.FromArgb(
+                (int)(vector.W * 255f),
+                (int)(vector.X * 255f),
+                (int)(vector.Y * 255f),
+                (int)(vector.Z * 255f)
+            );
+        }
+
+        /// <summary>
+        /// Converts a Byte3 color to System.Drawing.Color.
+        /// </summary>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static Color ToColor(this Byte3 vector, byte alpha = 255)
+        {
+            return Color.FromArgb(alpha, vector.X, vector.Y, vector.Z);
+        }
+
+        /// <summary>
+        /// Converts a Float3 color to System.Drawing.Color.
+        /// </summary>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static Color ToColor(this Float3 vector, float alpha = 1.0f)
+        {
+            return Color.FromArgb(
+                (int)(alpha * 255f),
+                (int)(vector.X * 255f),
+                (int)(vector.Y * 255f),
+                (int)(vector.Z * 255f)
             );
         }
     }
